69b7795d8715712786d2a63cf5e7aeb49068bc81
adcad022 <averchot@chicagobooth.edu>
2015-07-29 13:44:56 -0500

    Working

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,29 @@
 class GameController < ApplicationController
   # This action is for the bare domain. You can ignore it.
-  def home
-    redirect_to("/mockup.html")
+  def rps
+
+  @user_move = params["the_move"]
+
+  @computer_move = ["rock", "paper", "scissors"].sample
+
+  if @user_move == @computer_move
+  	@outcome = "tied"
+  elsif @user_move == "paper" && @computer_move == "rock"
+  	@outcome = "won"
+  elsif @user_move == "paper" && @computer_move == "scissors"
+  	@outcome = "lost"
+  elsif @user_move == "scissors" && @computer_move == "rock"
+  	@outcome = "lost"
+  elsif @user_move == "scissors" && @computer_move == "paper"
+  	@outcome = "won"
+  elsif @user_move == "rock" && @computer_move == "paper"
+  	@outcome = "lost"
+  elsif @user_move == "rock" && @computer_move == "scissors"
+  	@outcome = "won"
   end
 
-  # Your code goes below.
+  render("rps.html.erb")
+end
+
 
 end
